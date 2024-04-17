9b0112ad89b188ee7484e60b0ef60a591ac1ab37
codenamepsyzimz <harishankardas4274@gmail.com>
2024-04-17 06:20:24 +0530

    Error fixed on Product.jsx

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -4,7 +4,7 @@ const Product = ({product}) => {
   return (
     <Card className="my-3 p-3 rounded">
         <a href={'/product/${product._id}'}>
-            <Card.Img src={products.image} variant="top" />
+            <Card.Img src={product.image} variant="top" />
         </a>
 
         <Card.Body>
