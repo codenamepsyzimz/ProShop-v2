c221e064d4b464e151c6560a8f8f8eccb8449a5a
codenamepsyzimz <harishankardas4274@gmail.com>
2024-04-18 02:00:44 +0530

    Latest commit

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -2,11 +2,11 @@ import { useParams } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
 import Rating from '../components/Rating';
-import products from '../products' 
+import products from '../products';
 
 const ProductScreen = () => {
   const { id: productId } = useParams();
-  const product = products.find((p) => p._id === productId);
+  const product = products.find( (p) => p._id === productId);
 
   return (
     <>
